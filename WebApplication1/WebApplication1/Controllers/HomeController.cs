c95347790b922ba7db098d1ee4900ba75dea9d56
Mounika Reddy <Mounika Reddy>
2017-03-20 14:24:39 -0400

    First commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Stripe;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var result = GetResult();
+            return View();
+        }
+
+        public string GetResult()
+        {
+            BuySign(null);
+            return "";
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public static string BuySign(BuySignModel model)
+        {
+            var errorMessage = string.Empty;
+            var validationError = string.Empty;
+            var chargeId = string.Empty;
+
+            //if (ModelState.IsValid)
+            //{
+                //try
+                //{
+                    var tokenId = GetTokenId(model);
+                    chargeId = ChargeCustomer(tokenId);
+                //}
+                //catch (Exception e)
+                //{
+                //    errorMessage = e.Message;
+                //}
+            //}
+
+            return null;
+            //...rest of the code omitted for clarity
+        }
+
+        private static string ChargeCustomer(string tokenId)
+        {
+          
+                var myCharge = new StripeChargeCreateOptions
+                {
+                    Amount = 50,
+                    Currency = "gbp",
+                    Description = "Charge for property sign and postage",
+                    SourceTokenOrExistingSourceId = tokenId
+                };
+
+                var chargeService = new StripeChargeService();
+                var stripeCharge = chargeService.Create(myCharge);
+
+                return stripeCharge.Id;
+            
+        }
+
+        private static string GetTokenId(BuySignModel model)
+        {
+            
+                var myCardToken = new StripeCreditCardOptions
+                {
+                    //AddressCountry = model.CardAddressCountry,
+                    //AddressLine1 = model.CardAddressLine1,
+                    //AddressLine2 = model.CardAddressLine2,
+                    //AddressCity = model.CardAddressCity,
+                    //AddressZip = model.CardAddressZip,
+                    //Cvc = model.CardCvc.ToString(CultureInfo.CurrentCulture),
+                    //ExpirationMonth = model.CardExpirationMonth,
+                    //ExpirationYear = model.CardExpirationYear,
+                    //Name = model.CardName,
+                    //Number = model.CardNumber
+
+                    AddressCountry = "Brazil (BR)",
+                    AddressLine1 = " ",
+                    AddressLine2 = " ",
+                    AddressCity = " ",
+                    AddressZip = " ",
+                    Cvc = "954",
+                    ExpirationMonth = "1",
+                    ExpirationYear = "2020",
+                    Name = " ",
+                    Number = "4000000760000002"
+                };
+
+                var myToken = new StripeTokenCreateOptions
+                {
+                    Card = myCardToken
+                };
+                var tokenService = new StripeTokenService();
+                var stripeToken = tokenService.Create(myToken);
+
+                return stripeToken.Id;
+            
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/BuySignModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class BuySignModel
+    {
+        public string CardAddressCountry { get; set; }
+        public string CardAddressLine1 { get; set; }
+        public string CardAddressLine2 { get; set; }
+        public string CardAddressCity { get; set; }
+        public string CardAddressZip { get; set; }
+        public string CardCvc { get; set; }
+        public string CardExpirationMonth { get; set; }
+        public string CardExpirationYear { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
